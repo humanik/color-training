41858aa793b0a898051aeed13f2e34f85d55c160
humanik <humanikdev@gmail.com>
2020-02-03 09:10:56 +0400

    Deploy

---
+++ precache-manifest.f81a82e45f37010da17ef10d6b30833e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239591f39f29b6b8eb8c5411e2504771",
+    "url": "/color-training/audio/blue.mp3"
+  },
+  {
+    "revision": "1d2b1988f7d076731a115da3ea5c6b49",
+    "url": "/color-training/audio/green.mp3"
+  },
+  {
+    "revision": "7cb0d2872e4c72857191d5e23c6d8c09",
+    "url": "/color-training/audio/orange.mp3"
+  },
+  {
+    "revision": "8129b702d18c8f9d4a07ccdcc2e3962e",
+    "url": "/color-training/audio/red.mp3"
+  },
+  {
+    "revision": "8678eaae320cdbfc4fe5edf575bf46ab",
+    "url": "/color-training/audio/yellow.mp3"
+  },
+  {
+    "revision": "3845345db830e3492435",
+    "url": "/color-training/css/app.20f968a9.css"
+  },
+  {
+    "revision": "b60437d346d818a0f2a2",
+    "url": "/color-training/css/chunk-vendors.ee0edbad.css"
+  },
+  {
+    "revision": "16ea93888ad70aacdd7dbc8a70f8a26e",
+    "url": "/color-training/index.html"
+  },
+  {
+    "revision": "3845345db830e3492435",
+    "url": "/color-training/js/app.f0ead076.js"
+  },
+  {
+    "revision": "b60437d346d818a0f2a2",
+    "url": "/color-training/js/chunk-vendors.87286d46.js"
+  },
+  {
+    "revision": "44ac751d3bdd475b5a6e411540be9db0",
+    "url": "/color-training/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/color-training/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/color-training/precache-manifest.f81a82e45f37010da17ef10d6b30833e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "color-training"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
